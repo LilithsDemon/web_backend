1b80e76c8c5efdc0f4c1d48251a630210318fac4
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-11-04 15:18:45 +0000

    Simple test with jindium

--- Program.cs
+++ Program.cs
@@ -7,7 +7,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            int counter = 0;
+
             JinServer server = new JinServer();
+
+            server.ServerRoutes.AddStaticRoute("/hello", Method.GET, async(ctx) =>
+            {
+                await ctx.Send("Hello World!");
+            });
+
+            server.ServerRoutes.AddStaticRoute("/counter", Method.GET, async(ctx) =>
+            {
+                counter ++;
+
+                if(counter < 50)
+                {
+                    await ctx.Send(Convert.ToString(counter));
+                }
+                else
+                {
+                    await ctx.Send("You are a winner!");
+                    counter = 0;
+                }
+            });
 
             server.Start();
         }
