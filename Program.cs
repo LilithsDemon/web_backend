128aa6e3cd37889ac04e45511b93dab04ed926e5
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-11-04 14:41:37 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Jindium;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JinServer server = new JinServer();
+
+            server.Start();
+        }
+    }
+}
